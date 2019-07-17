f39522047fdca70445ef09a56a459ca9a5cd107d
Goldy Verma <749947@student.tor.loc>
2019-07-17 15:30:36 -0400

    2nd commit

--- shoppinglist/main.js
+++ shoppinglist/main.js
@@ -3,3 +3,10 @@ const url = require('url');
 const path = require('path');
 
 const {app, BrowserWindow} = electron;
+
+let mainWindow;
+ 
+//Listen 
+app.on('ready',function(){
+    mainWindow = new BrowserWindow({});
+})
